302768dd9b4418657b51f69f027267cc2a7c4f15
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2023-05-29 16:14:20 +0200

    <leader>t will toggle neotree

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -31,6 +31,7 @@ return {
 		},
 		config = function()
 			-- Unless you are still migrating, remove the deprecated commands from v1.x
+			vim.keymap.set('n', '<leader>t', '<Cmd>Neotree toggle<CR>', { desc = '[t] Toggle NeoTree' })
 			vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 			require('neo-tree').setup {}
 		end,
